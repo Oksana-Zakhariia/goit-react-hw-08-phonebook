84862e967e8248e5a3b94c5369e497f2495dd381
Oksana-Zakhariia <okcahka.l@gmail.com>
2023-03-06 20:37:09 +0200

    add useEffect to Contacts

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,14 +1,24 @@
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { SearchBox } from 'components/SearchBox/Searchbox';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from 'redux/contacts/operations';
+import { getIsLoading } from 'redux/contacts/selectors';
 
 export default function Contacts() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
     <div>
       <h1>Here you can add or remote your contacts</h1>
 
       <ContactForm></ContactForm>
       <SearchBox></SearchBox>
+      <div>{isLoading && 'Request in progress...'}</div>
       <ContactList />
     </div>
   );
